b7c2cee9d17c5a11d9d26a0feb5c441f80383f70
imetandy <robinson.andrewjames@gmail.com>
2023-12-05 20:09:01 -0500

    rebuilt anchor-lang-docs folder

--- rs/anchor-lang-docs/programs/anchor-lang-docs/src/lib.rs
+++ rs/anchor-lang-docs/programs/anchor-lang-docs/src/lib.rs
@@ -1,65 +1,50 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::TokenAccount;
 
-declare_id!("9vKqa7W8GfZiCsE6SicAZuiDPbDqquU4wTsp1zfg7VYE");
+declare_id!("G97vWGkrWRCDDnP1MaNJDLF5cewWjXv6NP23hoADRPvf");
 
 #[program]
 pub mod anchor_lang_docs {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>, data: MyAccount) -> Result<()> {
-        ctx.accounts.my_account.data  = data.data;
-        ctx.accounts.my_account.age = data.age;
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.my_account.data = 0;
         Ok(())
     }
 
 
-//                               1          5         4
-    pub fn set_data(ctx: Context<SetData>, data: MyAccount) -> Result<()> {
-        require!(data.data < 100, MyError::DataTooLarge);
-        //   1      2         3                 5
-            ctx.accounts.my_account.set_inner(data);
-            Ok(())
-        } 
+
+    pub fn set_data(ctx: Context<SetData>, data: u64) -> Result<()> {
+        ctx.accounts.my_account.data = data;
+        Ok(())
     }
+}
 
 
 #[account]
-// The data account
 #[derive(Default)]
-//               4
 pub struct MyAccount {
-//    5
-    data: u64,
-    age: u8
+    pub data: u64
 }
-
-
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = owner, space = 8 + 8 + 32 + 1)]
+    #[account(
+        init, 
+        payer = payer, 
+        space = 8 + 8)]
     pub my_account: Account<'info, MyAccount>,
     #[account(mut)]
-    pub owner: Signer<'info>,
-    pub system_program: Program<'info, System>,
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>, 
 }
-
 
 #[derive(Accounts)]
-//           1
 pub struct SetData<'info> {
-    // The account to update
-    //   2  
-    #[account(mut)]
-    //      3                         4
+    #[account(mut, seeds = [
+        b"my_account".as_ref(),
+        my_account.key().as_ref()
+    ], bump)]
     pub my_account: Account<'info, MyAccount>,
     #[account(mut)]
-    pub owner: Signer<'info>,
+    pub payer: Signer<'info>,
 }
 
-#[error_code]
-pub enum MyError {
-    #[msg("MyAccount may only hold data less than 100")]
-    DataTooLarge
-}
--- rs/anchor-lang-docs/tests/anchor-lang-docs.ts
+++ rs/anchor-lang-docs/tests/anchor-lang-docs.ts
@@ -1,56 +1,61 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorLangDocs } from "../target/types/anchor_lang_docs";
-import { expect } from "chai";
 
 describe("anchor-lang-docs", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.getProvider();
+  const myAccount = anchor.web3.Keypair.generate();
+  const payer = anchor.web3.Keypair.generate();
 
   const program = anchor.workspace.AnchorLangDocs as Program<AnchorLangDocs>;
-  const myAccount = anchor.web3.Keypair.generate();
-  const tokenAccount = anchor.web3.Keypair.generate();
-  const signer = anchor.web3.Keypair.generate();
+  it("Airdrop Tokens", async () => {
+    const tx1 = await provider.connection.requestAirdrop(
+      payer.publicKey,
+      anchor.web3.LAMPORTS_PER_SOL*10
+    );
+    const tx2 = await provider.connection.requestAirdrop(
+      myAccount.publicKey,
+      anchor.web3.LAMPORTS_PER_SOL*5
+    );
+    await provider.connection.confirmTransaction(tx1);
+    await provider.connection.confirmTransaction(tx2);
+    
+    // log payer sol amount
+    const payerBalance = await provider.connection.getBalance(payer.publicKey);
+    const myAccountBalance = await provider.connection.getBalance(myAccount.publicKey);
+    console.log("Payer balance:", payerBalance);
+    console.log("My account balance:", myAccountBalance);
+  });
 
   it("Is initialized!", async () => {
-    // Add your test here.
-
-    const tx = await program.methods
-    .initialize({
-      data: new anchor.BN(5),
-      age: new anchor.BN(1)
-    })
+    const tx = await program.methods.initialize()
     .accounts({
-      myAccount: myAccount.publicKey,
-      owner: signer.publicKey,
+        myAccount: myAccount.publicKey,
+        payer: payer.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
     })
-    .signers([signer])
-    .rpc();
+    .signers([payer]).rpc();
 
-    const account = await program.account.myAccount.fetch(myAccount.publicKey);
-    expect(account.data).to.equal("MyAccount");
+    // log the new data value on my account
+    const myAccountData = (await program.account.myAccount.fetch(myAccount.publicKey)).data;
+    console.log("My account data:", myAccountData.toString());
+    console.log("Your transaction signature", tx);
+    });
+
+
+    // Updating the data 
+    xit("Update data!", async () => {
+      const tx = await program.methods.setData(new anchor.BN(100)).accounts(
+        {
+          myAccount: myAccount.publicKey,
+          payer: payer.publicKey,
+        }
+      ).signers([payer]).rpc();
+      const myAccountData = (await program.account.myAccount.fetch(myAccount.publicKey)).data;
+      console.log("My account data:", myAccountData.toString());
+      console.log("Your transaction signature", tx);
+
   });
-
-  it("Set Data", async () => {
-    // Add your test here.
-
-    const tx = await program.methods
-    .setData({
-      data: new anchor.BN(5),
-      age: new anchor.BN(1)
-    })
-    .accounts({
-      myAccount: myAccount.publicKey,
-      owner: signer.publicKey,
-    })
-    .signers([signer])
-    .rpc();
-
-    const account = await program.account.myAccount.fetch(myAccount.publicKey);
-    expect(account.data).to.equal("MyAccount");
-  });
-
-
-
-
 });
