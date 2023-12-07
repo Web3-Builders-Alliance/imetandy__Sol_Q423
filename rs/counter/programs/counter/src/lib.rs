a097bd070155e23a25ea94b1beb315b28bbeb1be
imetandy <robinson.andrewjames@gmail.com>
2023-12-06 20:33:48 -0500

    Counter Refactor

--- rs/counter/programs/counter/src/lib.rs
+++ rs/counter/programs/counter/src/lib.rs
@@ -6,21 +6,18 @@ declare_id!("DPXqfPv4i5NkU4ENmgQLAfD2LWKpYLyzxFFsKaFsCstm");
 pub mod counter {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, count_value: u64) -> Result<()> {
-        ctx.accounts.counter.count = count_value;
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.counter.count = 0;
         ctx.accounts.counter.bump = ctx.bumps.counter;
-        msg!("Counter Initialized");
         Ok(())
     }
 
     pub fn increment(ctx: Context<Count>, count_value: u64) -> Result<()> {
         ctx.accounts.counter.count += count_value;
-        msg!("Counter Increased to: {}", ctx.accounts.counter.count);
         Ok(())
     }
     pub fn decrement(ctx: Context<Count>, count_value: u64) -> Result<()> {
         ctx.accounts.counter.count -= count_value;
-        msg!("Counter Decreased to: {}", ctx.accounts.counter.count);
         Ok(())
     
     }
@@ -28,8 +25,6 @@ pub mod counter {
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(mut)]
-    pub signer: Signer<'info>,
     #[account(
         init,
         payer = signer, 
@@ -41,13 +36,13 @@ pub struct Initialize<'info> {
             space = Counter::INIT_SPACE,
     )]
     pub counter: Account<'info, Counter>,
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct Count<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+ 
+#[derive(Accounts)]
+pub struct Count<'info> {
     #[account(
         mut, 
         seeds = [
@@ -57,9 +52,12 @@ pub struct Count<'info> {
             bump = counter.bump,
     )]
     pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
 }
 
 #[account]
+#[derive(Default)]
 pub struct Counter {
     pub count: u64,
     pub bump: u8,
@@ -69,4 +67,3 @@ pub struct Counter {
 impl Space for Counter {
     const INIT_SPACE: usize = 8 + 8 + 1; // Anchor descriminator + counter + bump
 }
-
--- rs/counter/tests/counter.ts
+++ rs/counter/tests/counter.ts
@@ -1,49 +1,69 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Counter } from "../target/types/counter";
+import { findProgramAddressSync } from "@project-serum/anchor/dist/cjs/utils/pubkey";
 
 describe("Counter", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.Counter as anchor.Program<Counter>;
-  const counter = anchor.web3.Keypair.generate();
+  
   const provider = anchor.getProvider();
+  const connection = provider.connection;
   const signer = anchor.web3.Keypair.generate();
-
-  let countValue = 0;
-
+  const counter = findProgramAddressSync([Buffer.from("counter"), signer.publicKey.toBytes()] , program.programId)[0];
+  
   const confirm = async (signature: string): Promise<string> => {
-    const block = await provider.connection.getLatestBlockhash();
-    await provider.connection.confirmTransaction({
+    const block = await connection.getLatestBlockhash();
+    await connection.confirmTransaction({
       signature,
       ...block
-    });
-    return signature;
+    })
+    return signature
   }
+  
+    const log = async(signature: string): Promise<string> => {
+      console.log(`Your transaction signature: https://explorer.solana.com/transaction/${signature}?cluster=custom&customUrl=${connection.rpcEndpoint}`);
+      return signature;
+    }
 
-  it("Airdrop 1 SOL!", async () => {
-    const tx = await provider.connection.requestAirdrop(counter.publicKey, 1e9).then(confirm);
-  });
+  it("Airdrop Tokens", async () => {
+    const tx1 = await provider.connection.requestAirdrop(
+      signer.publicKey,
+      anchor.web3.LAMPORTS_PER_SOL*10
+    );
+    await provider.connection.confirmTransaction(tx1);
+    });
+
   it("Initialize", async () => {
-    const tx = await program.methods.initialize(new anchor.BN(countValue)).accounts({
+    const tx = await program.methods.initialize()
+    .accounts({
       signer: signer.publicKey,
-      counter: counter.publicKey,
-      systemProgram: program.programId,
-    });    
+      counter: counter,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).signers([signer]).rpc().then(confirm).then(log);    
   });  
 
   it("Add x to counter", async () => {
-    const tx = await program.methods.increment(new anchor.BN(5));
-    const account = await program.account.counter.fetch(counter.publicKey);
-    console.log(account);
+    const tx = await program.methods.increment(new anchor.BN(5))
+    .accounts({
+      counter: counter,
+      signer: signer.publicKey,
+      }).signers([signer]).rpc().then(confirm).then(log);
+    const account = (await program.account.counter.fetch(counter)).count;
+    console.log(account.toNumber());
   });
 
   it("Remove x from counter", async () => {
-    const tx = await program.methods.decrement(new anchor.BN(5));
-    let countValue = (await program.account.counter.fetch(counter.publicKey)).count.toNumber();
-    console.log(countValue);
+    const tx = await program.methods.decrement(new anchor.BN(5))
+    .accounts({
+      signer: signer.publicKey,
+        counter: counter,
+        }).signers([signer]).rpc().then(confirm).then(log);
+    let countValue = (await program.account.counter.fetch(counter)).count;
+    console.log(countValue.toNumber());
     ;
   });
 
-
 });
+
